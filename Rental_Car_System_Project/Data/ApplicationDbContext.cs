11f32028809f3a1416c447de1f05287c243940e7
KostadinBundov <kostadinbundov@gmail.com>
2023-03-25 10:39:10 +0200

    Add project files.

---
+++ Rental_Car_System_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rental_Car_System_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Rental_Car_System_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rental_Car_System_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
